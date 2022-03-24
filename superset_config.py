f26dd857e7553325f6d881d55a74e9c6201abe19
Baysan <46582456+mebaysan@users.noreply.github.com>
2022-03-24 17:00:12 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,1 +1,11 @@
 SQLALCHEMY_DATABASE_URI = 'postgresql://USERNAME:PASSWORD@HOST:PORT/DB_NAME'
+
+
+# Uncomment to setup Your App name
+#APP_NAME = "BaysanSoft BI"
+
+# Specify the App icon
+#APP_ICON = "/static/assets/images/XYZ.png"
+#APP_ICON_WIDTH = 63
+
+#FAVICONS = [{"href": "/static/assets/images/XYZ.png"}]
